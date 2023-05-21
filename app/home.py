278589da07903ffcef867c3f8fa111fe9803dec2
camlebrun <c_lebrun3@hetic.eu>
2023-05-21 19:03:22 +0200

    Resoloves 9: streamlit EDA

---
+++ app/home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.set_page_config(page_title='France Echanges', page_icon=':house:', layout='wide')
+
+st.markdown("<h1 style='text-align: center;'>France Echanges</h1>", unsafe_allow_html=True)
+
+st.write("Bienvenue sur France Echanges, Ensemble, construisons le service public de l'emploi du futur en donnant la parole aux usagers")
+st.write("Pour commencer, veuillez sélectionner une page dans le menu de gauche")
+st.write("Blablabla")
