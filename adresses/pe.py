3d0e8af4158aa5551d4ca6ae33f6e9beb81b80f3
camlebrun <c_lebrun3@hetic.eu>
2023-05-23 17:39:26 +0200

    rgpd

--- adresses/pe.py
+++ adresses/pe.py
@@ -1,29 +1,32 @@
-import requests
-import csv
+import pandas as pd
+import os
 
-url = "https://etablissements-publics.api.gouv.fr/v3/organismes/pole_emploi"
-params = {}
+# Charger le fichier CSV contenant les données de scraping
+data_file = '/Users/camille/repo/Hetic/projet_gouv/scraping/output/google_reviews.csv'
+df_scraped_data = pd.read_csv(data_file)
 
-response = requests.get(url, params=params)
-data = response.json()
+# Chemin du fichier contenant les adresses
+addresses_file = '/Users/camille/repo/Hetic/projet_gouv/scraping/pole_emploi.csv'
 
-# Vérifier si la requête a réussi
-if response.status_code == 200:
-    # Ouvrir un fichier CSV en mode écriture
-    with open("pole_emploi.csv", "w", newline="", encoding="utf-8") as csvfile:
-        writer = csv.writer(csvfile)
+# Charger les adresses depuis le fichier
+df_addresses = pd.read_csv(addresses_file)
 
-        # Écrire l'en-tête du CSV
-        writer.writerow(["Nom", "Adresse"])
+# Obtenir tous les noms des lieux
+all_places = df_addresses['Adresse'].tolist()
 
-        # Parcourir les données et extraire le nom et l'adresse
-        for feature in data["features"]:
-            for elem in feature:
-                properties = elem["properties"]
-                nom = properties["nom"]
-                adresse = properties["adresses"][0]["lignes"][0]
-                writer.writerow([nom, adresse])
+# Obtenir les noms des lieux présents dans le DataFrame de scraping
+scraped_places = df_scraped_data['object_name'].tolist()
 
-    print("Le fichier CSV a été créé avec succès.")
-else:
-    print("La requête a échoué avec le code d'erreur :", response.status_code)
+# Trouver les lieux qui n'ont pas été scrapés
+missing_places = list(set(all_places) - set(scraped_places))
+num_missing_places = len(missing_places)
+
+# Enregistrer les noms des lieux non scrapés et le nombre d'objets non scrapés dans un fichier texte
+output_file = '/Users/camille/repo/Hetic/projet_gouv/scraping/output/missing_places.txt'
+with open(output_file, 'w') as f:
+    f.write("Lieux manquants :\n")
+    for place in missing_places:
+        f.write(place + '\n')
+    f.write(f"\nNombre d'objets non scrapés : {num_missing_places}")
+
+print("Les lieux manquants ont été enregistrés dans le fichier 'missing_places.txt'.")
