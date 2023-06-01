1aca9c5d9076570825c8ccbf151de87363a28e1a
camlebrun <c_lebrun3@hetic.eu>
2023-06-01 16:48:33 +0200

    pause

--- app/pages/4_Lieux.py
+++ app/pages/4_Lieux.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-import spacy
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 from nltk import ngrams
